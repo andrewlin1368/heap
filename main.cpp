4ac7f96dd3a63890af76759a585363e8a0ad1296
andrewlin1368 <andrewlin1368@gmail.com>
2021-01-30 20:54:06 -0500

    main

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "heap.h"
 
 int main(){
-    int arr[7]={0,1,2,3,4,5,6};
+    int arr[8]={0,1,2,3,4,5,6,7};
     heap h;
-    for(int i=0;i<7;i++)
+    for(int i=0;i<8;i++)
         h.insert(arr[i]);
+    cout<<"Before: ";
     h.output();
-    cout<<"\n";
+    cout<<"\nAfter: ";
     h.heapify();
     h.output();
     return 0;
