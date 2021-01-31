196f37dfef36404ee2d5aaeb561eaa63fbce2f0b
andrewlin1368 <andrewlin1368@gmail.com>
2021-01-30 20:58:05 -0500

    added remove function call

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 #include "heap.h"
 
 int main(){
-    int arr[8]={0,1,2,3,4,5,6,7};
+    int arr[7]={0,1,2,3,4,5,6};
     heap h;
-    for(int i=0;i<8;i++)
+    for(int i=0;i<7;i++)
         h.insert(arr[i]);
     cout<<"Before: ";
     h.output();
     cout<<"\nAfter: ";
     h.heapify();
+    h.output();
+    cout<<"\nAfter remove: ";
+    h.remove();
     h.output();
     return 0;
 }
